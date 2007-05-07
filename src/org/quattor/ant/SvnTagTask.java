c8240470660dac6993fb9d6e4ed5158b44d77c10
jouvin <jouvin@60caac41-0bef-0310-ae6f-f8fb43407fe4>
2007-05-07 13:00:46 +0000

    SvnTagTask : add support for new tag format (hierarchical tags like /YYYY/MM/DDHHMMSS)
    git-svn-id: http://svn.lal.in2p3.fr/LCG/QWG/scdb-ant-utils/trunk@1684 60caac41-0bef-0310-ae6f-f8fb43407fe4

--- src/org/quattor/ant/SvnTagTask.java
+++ src/org/quattor/ant/SvnTagTask.java
@@ -1,15 +1,23 @@
 package org.quattor.ant;
 
 import java.io.File;
+import java.util.*;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+
+import org.tmatesoft.svn.core.SVNDirEntry;
 import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNNodeKind;
 import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
 import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl;
 import org.tmatesoft.svn.core.wc.ISVNStatusHandler;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNCommitClient;
 import org.tmatesoft.svn.core.wc.SVNCopyClient;
 import org.tmatesoft.svn.core.wc.SVNInfo;
 import org.tmatesoft.svn.core.wc.SVNRevision;
@@ -17,6 +25,7 @@ import org.tmatesoft.svn.core.wc.SVNStatus;
 import org.tmatesoft.svn.core.wc.SVNStatusClient;
 import org.tmatesoft.svn.core.wc.SVNStatusType;
 import org.tmatesoft.svn.core.wc.SVNWCClient;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 /**
  * This ant task will create a new tag in the repository of the current
@@ -95,10 +104,11 @@ public class SvnTagTask extends Task {
 		SVNStatusClient status = manager.getStatusClient();
 		SVNCopyClient copy = manager.getCopyClient();
 		SVNWCClient wc = manager.getWCClient();
+		SVNCommitClient commit = manager.getCommitClient();
 
 		// Create a handler to collect the information.
 		StatusHandler handler = new StatusHandler();
-
+		
 		// Retrieve the URL for the repository.
 		SVNInfo info = null;
 		try {
@@ -108,30 +118,52 @@ public class SvnTagTask extends Task {
 		}
 
 		SVNURL srcUrl = info.getURL();
-		String url = srcUrl.toString();
+		String tagsBranch = srcUrl.toString();
 
 		// Verify that the working copy is actually from the trunk.
-		if (!url.endsWith("trunk")) {
+		if (!tagsBranch.endsWith("trunk")) {
 			throw new BuildException(
-					"working copy must be from repository trunk: " + url);
+					"working copy must be from repository trunk: " + tagsBranch);
 		}
 
 		// Parse the source URL to determine the URL for the tag.
-		int i = url.lastIndexOf("/");
+		String tagPath = null;
+		int i = tagsBranch.lastIndexOf("/");
 		if (i >= 0) {
-			url = url.substring(0, i) + "/tags/" + tag;
+			tagsBranch = tagsBranch.substring(0, i) + "/tags/";
+			tagPath = tagsBranch + tag;
 		} else {
-			throw new BuildException("found invalid SVN URL: " + url);
+			throw new BuildException("found invalid SVN URL: " + tagsBranch);
 		}
-
-		// Create the destination URL from the new string value.
-		SVNURL destUrl = null;
+		SVNURL tagUrl = null;
 		try {
-			destUrl = SVNURL.parseURIDecoded(url);
+			tagUrl = SVNURL.parseURIEncoded(tagPath);
 		} catch (SVNException e) {
-			throw new BuildException("error creating dest. URL: " + url);
+			throw new BuildException("Error parsing tag URL "+tagPath+": "+e.getMessage());
 		}
 
+		// Create a repository instance for tags
+		// Check tag branch root exists
+		SVNRepository repositoryTags = null;
+		try {
+			repositoryTags = SVNRepositoryFactory.create(SVNURL.parseURIEncoded(tagsBranch));
+		} catch (SVNException e) {
+			throw new BuildException(
+					"Error creating SVNRepository instance for location "+tagsBranch+": "+e.getMessage());
+		}
+		ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager();
+		repositoryTags.setAuthenticationManager(authManager);
+		try {
+			SVNNodeKind tagNodeKind = repositoryTags.checkPath("",-1);
+			if ( tagNodeKind != SVNNodeKind.DIR ) {
+				throw new BuildException(
+						"Error : SVN branch "+tagsBranch+" must exist and be a directory");
+			}
+		} catch ( SVNException e ) {
+			throw new BuildException(
+					"Error getting information about SVN branch "+tagsBranch+": "+e.getMessage());
+		}
+		
 		// Check for any local modifications. With the flags below,
 		// it will do a recursive search of the workspace path but
 		// only use local information. The handler will not be called
@@ -168,9 +200,60 @@ public class SvnTagTask extends Task {
 		}
 
 		// Actually make the tag.
+		// First check the tag branch exists and create it if necessary.
+		// The check needs to be done recursively as doMkDir() doesn't allow
+		// to create intermediate branch levels implicitly.
 		System.out.println("Making tag: " + tag);
+		LinkedList<String> branchesToCreate = new LinkedList<String>();
+		boolean tagDirExists = false;
+		String tagParent;
+		int parentIndex = tag.lastIndexOf("/");
+		i = parentIndex;
+		while ( !tagDirExists && (i>0)) {
+			SVNNodeKind tagParentNodeKind = null;
+			tagParent = tag.substring(0,i);
+			try {
+				tagParentNodeKind = repositoryTags.checkPath(tagParent, -1);
+			} catch (SVNException e) {
+				throw new BuildException(
+						"Error checking existence of SVN branch "+tagParent+": "+e.getMessage());
+			}
+			if ( tagParentNodeKind == SVNNodeKind.NONE ) {
+				branchesToCreate.addFirst(tagsBranch+tagParent);
+				i = tag.lastIndexOf("/");
+			} else if ( tagParentNodeKind != SVNNodeKind.DIR ) {
+				throw new BuildException(
+						"Error: "+tagParent+" exists in repository but is not a directory");
+			} else {
+				tagDirExists = true;
+			}
+		}
+		if ( ! branchesToCreate.isEmpty() ) {
+			SVNURL[] urlsToCreate = new SVNURL[branchesToCreate.size()];
+			int j = 0;
+			for (Iterator<String> it=branchesToCreate.iterator(); it.hasNext(); ) {
+				String branchPath = it.next(); 
+				try {
+					urlsToCreate[j] = SVNURL.parseURIEncoded(branchPath);
+				} catch ( SVNException e) {
+					throw new BuildException(
+							"Error converting "+branchPath+" to URL:"+e.getMessage());
+				}
+				j++;
+			}
+			try {
+				commit.doMkDir(urlsToCreate,"SCDB ant tools : create new tag branch");
+			} catch (SVNException e) {
+				String tagRoot = tagsBranch;
+				if ( parentIndex > 0 ) {
+					tagRoot += tag.substring(0,parentIndex);
+
+				}
+				throw new BuildException("Error creating tag branch "+tagRoot+": " + e.getMessage());
+			}
+		}
 		try {
-			copy.doCopy(srcUrl, SVNRevision.HEAD, destUrl, false, "ant tag");
+			copy.doCopy(srcUrl, SVNRevision.HEAD, tagUrl, false, "ant tag");
 		} catch (SVNException e) {
 			throw new BuildException("tag failed: " + e.getMessage());
 		}
@@ -226,3 +309,4 @@ public class SvnTagTask extends Task {
 	}
 
 }
+
