d32a9e51582d42d9d5509a5a83367de9947e6023
jouvin <jouvin@60caac41-0bef-0310-ae6f-f8fb43407fe4>
2007-06-18 08:14:14 +0000

    Fix debug message
    git-svn-id: http://svn.lal.in2p3.fr/LCG/QWG/scdb-ant-utils/trunk@1872 60caac41-0bef-0310-ae6f-f8fb43407fe4

--- src/org/quattor/ant/SvnTagTask.java
+++ src/org/quattor/ant/SvnTagTask.java
@@ -319,7 +319,8 @@ public class SvnTagTask extends Task {
 					&& (s != SVNStatusType.STATUS_EXTERNAL);
 			
 			if ( debugHandler ) {
-				System.out.println("File="+status.getFile()+" ("+status.getURL().toString()+"), Status="+status.toString());
+//				System.out.println("File="+status.getFile()+" ("+status.getURL().toString()+"), Status="+status.toString());
+				System.out.println("File="+status.getFile()+", Status="+s.toString());
 			}
 
 			
