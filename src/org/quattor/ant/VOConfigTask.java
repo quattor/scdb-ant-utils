828bc6f7d4d53f559f0fdb5b7158a50b70c9f3c9
jouvin <jouvin@60caac41-0bef-0310-ae6f-f8fb43407fe4>
2010-12-16 09:57:56 +0000

    New VOConfigTask: skeleton improvements
    git-svn-id: http://svn.lal.in2p3.fr/LCG/QWG/scdb-ant-utils/trunk@5343 60caac41-0bef-0310-ae6f-f8fb43407fe4

--- src/org/quattor/ant/VOConfigTask.java
+++ src/org/quattor/ant/VOConfigTask.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -52,7 +53,7 @@ public class VOConfigTask extends Task {
 	 * This is a hash with one entry per VO : the key is the VO name.
 	 */
 	private Hashtable<String,VOConfig> voMap = null;
-	
+
 	// Methods
 
 	/**
@@ -132,7 +133,7 @@ public class VOConfigTask extends Task {
 
 	@Override
 	public void execute() throws BuildException {
-		
+
 		// Parse VO ID cards
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
@@ -161,29 +162,51 @@ public class VOConfigTask extends Task {
 	}
 
 
+	private enum DataContexts {
+		DATA_IGNORED,
+		DATA_VOMS_HOST,
+		DATA_VOMS_HOST_PORT,
+		DATA_VOMS_VOMS_PORT,
+		DATA_VOMS_CERT,
+		DATA_VOMS_DN
+	}
+
 	// SAX content handler for VO cards
-	
+
 	public class VOCardHandler extends DefaultHandler {
-		
+
 		/* VO currently being processed */
 		private String voName = null;
 		private VOConfig voConfig = null;
 
+		/* Hash table of all defined VOMS servers.
+		 * Used to check consistency of VOMS server attributes across VOs.
+		 */
+		private Hashtable<String,VOMSServer>vomsServers = null;
 		
+		/* Context variables */
+		private boolean sectionVOMSServers = false;
+		private VOMSServer vomsServer = null;
+		private VOMSServerEndpoint vomsServerEndpoint = null;
+		private DataContexts dataContext = DataContexts.DATA_IGNORED;
+
+
 		/*
 		 * Start of document
 		 */
-		
+
 		@Override
 		public void startDocument () throws SAXException {
 			voMap = new Hashtable<String,VOConfig>();
+			vomsServers = new Hashtable<String,VOMSServer>();
+			dataContext = DataContexts.DATA_IGNORED;
 		}
-		
-		
+
+
 		/**
 		 * Start of new element
 		 */
-		
+
 		@Override
 		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 			if ( qName.equals("VO") ) {
@@ -199,13 +222,25 @@ public class VOConfigTask extends Task {
 					throw new SAXException("Invalid configuration: VO has no Id");
 				}
 				voConfig.setId(Integer.parseInt(voId));
+
+			} else if ( qName.equals("VOMSServers") ) {
+				sectionVOMSServers = true;
+				voConfig.vomsServerList = new LinkedList<VOMSServerEndpoint>();
+
+			} else if ( sectionVOMSServers ) {
+				if ( qName.equals("VOMSServer") ) {
+					vomsServer = new VOMSServer();		
+					vomsServerEndpoint = new VOMSServerEndpoint();
+				} else if ( qName.equals("HOSTNAME") ) {
+					dataContext = DataContexts.DATA_VOMS_HOST;
+				}
 			}
 		}
-		
+
 		/**
 		 * End of an element
 		 */
-		
+
 		@Override
 		public void endElement(String uri, String localName, String qName) throws SAXException {
 			if ( qName.equals("VO") ) {
@@ -213,7 +248,7 @@ public class VOConfigTask extends Task {
 					try {
 						voMap.put(voName, voConfig);
 					} catch (NullPointerException e) {
-						throw new SAXException("Internal error: voConfig undefined at the end of VO "+voName+" configuration");
+						throw new SAXException("Internal error: voMap or voConfig undefined at the end of VO "+voName+" configuration");
 					}
 					if ( debugTask ) {
 						System.out.println("Finished processing VO "+voName);						
@@ -221,27 +256,118 @@ public class VOConfigTask extends Task {
 				} else {
 					throw new SAXException("Parsing error: end of VO configuration found before start");
 				}
+			} else if ( qName.equals("VOMSServers") ) {
+				sectionVOMSServers = false;
+			} else if ( sectionVOMSServers ) {
+				// Check the VOMS server has not been defined yet by another VO or that attributes are consistent
+				if ( qName.equals("VOMSServer") ) {
+					if ( vomsServers.contains(vomsServer.host) ) {
+						if ( debugTask ) {
+							System.err.println("VOMS server '"+vomsServer.host+"' already defined: checking attribute consistency.");
+						}
+					} else {
+						if ( debugTask ) {
+							System.err.println("Adding VOMS server '"+vomsServer.host+"' to global VOMS server list.");
+						}
+						vomsServers.put(vomsServer.host,vomsServer);
+					}
+					vomsServerEndpoint.server = vomsServer;
+					voConfig.vomsServerList.add(vomsServerEndpoint);					
+				}
 			}			
 		}
-	}
 
-	
+		/*
+		 * Retrieve element data
+		 */
+		
+		@Override
+		public void characters (char[] chars, int start, int length) throws SAXException {
+			if ( dataContext != DataContexts.DATA_IGNORED) {
+				String data = new String(chars,start,length);
+				switch (dataContext) {
+				case DATA_IGNORED:
+					vomsServer.host = data;
+					break;
+				}
+			}
+		}
+
+	}
+		
+
 	// Class representing a VO
-	
+
 	private class VOConfig {
 		/* VO ID number */
-		int id = 0;
-		
-		
+		private int id = 0;
+
+		/* List of VOMS servers */
+		private LinkedList<VOMSServerEndpoint> vomsServerList = null;
+
+
 		// Methods
 
 		private int getId() {
 			return (this.id);
 		}
-		
+
 		private void setId(int id) {
 			this.id = id;
 		}
-		
+
+	}
+
+
+	// Class representing a VOMS server endpoint (used by a specific V0)
+	
+	private class VOMSServerEndpoint {
+		private VOMSServer server = null;
+		private int port;
+		private String endpoint = null;
+	}
+	
+	
+	// Class representing a VOMS server
+
+	private class VOMSServer {
+		private String host = null;
+		private int port = 8443;
+		private String cert = null;
+		private boolean vomsAdminEnabled = true;
+
+		// Methods
+
+		private String getCert() {
+			return (this.cert);
+		}
+
+		private String getHost() {
+			return (this.host);
+		}
+
+		private int getPort() {
+			return (this.port);
+		}
+
+		private boolean getVomsAdminEnabled() {
+			return (this.vomsAdminEnabled);
+		}
+
+		private void setCert(String cert) {
+			this.cert = cert;
+		}
+
+		private void setHost(String host) {
+			this.host = host;
+		}
+
+		private void setPort(int port) {
+			this.port = port;
+		}
+
+		private void setVomsAdminEnabled(boolean vomsAdminEnabled) {
+			this.vomsAdminEnabled = vomsAdminEnabled;
+		}
 	}
 }
